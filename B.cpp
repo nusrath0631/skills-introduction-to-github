64f2a9f22735253d37b3aabb8941ec8079868a0a
nusrath0631 <nusrathjahanmahi06@gmail.com>
2025-07-27 21:44:07 +0600

    Add files via upload

--- B.cpp
+++ B.cpp
@@ -1,14 +1,29 @@
-#include<bits/stdc++.h>
+//#include<bits\stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    string line;
-    while(getline(cin,line)){
-        for(int i=0; i<line.length(); i++)
-        {
-            line[i] = line[i]-7;
+    int T;
+    cin>>T;
+    for(int num=1; num<=T; num++){
+        string a;
+        int b;
+        cin>>a>>b;
+        long long rem=0;
+        int i = 0;
+        if(a[0]=='-'){
+            i = 1;
         }
-        cout<<line<<endl;
+        for (i=1; i < a.size(); ++i) {
+            rem = (rem * 10 + (a[i] - '0')) % abs(b);
+        }
+        cout << "Case " << num << ": ";
+        if (rem == 0)
+            cout << "divisible" << endl;
+        else
+            cout << "not divisible" << endl;
+
     }
     return 0;
 }
